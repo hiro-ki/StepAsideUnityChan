e6af4f5f035dd808f665dd4e8469b1e410ba086b
HirokiYamamoto <futsalhiro@gmail.com>
2017-01-24 21:47:07 -0800

    Lesson6課題
    課題のレビューお願いします

---
+++ Assets/Destroy.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroy : MonoBehaviour {
+
+	private GameObject unitychanObject;
+	// Use this for initialization
+	void Start () {
+		unitychanObject = GameObject.Find ("unitychan");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (gameObject.transform.position.z + 3 < unitychanObject.transform.position.z) {
+			Destroy (gameObject);
+		}
+	}
+}
